950eaa005ad061e5373ca1d05f9d0a9ba2262fdd
Sailesh Gurung <saileshgurung98@gmail.com>
2025-06-03 22:04:46 +0545

    day 03

--- Day03/hello.js
+++ Day03/hello.js
@@ -1,7 +1,7 @@
-const mission = 'Learn'
+const mission = 'learn'
 
 if( mission === 'Learn'){
     console.log('Time To write some node code');
 } else {
-    console.log('Is' + mission + 'really more fun?')
+    console.log(`Is ${mission} really more fun?`)
 }
