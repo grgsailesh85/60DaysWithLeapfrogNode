1145e5b8f3ddd022f007228a4bd04faeea5d69e8
Sailesh Gurung <saileshgurung98@gmail.com>
2025-06-09 21:12:19 +0545

    Day08

--- Day08/request.js
+++ Day08/request.js
@@ -1,13 +1,17 @@
+// module.exports.REQUEST_TIMEOUT = 5000; // 5 seconds
+exports.REQUEST_TIMEOUT = 5000; // 5 seconds
+
 function encrypt(data) {
   return "encrypted data";
 }
 
-function send(url, data) {
+// module.exports.send = function send(url, data) {
+exports.send = function send(url, data) {
   const encryptedData = encrypt(data);
-  console.log(`sending ${encryptedData} to ${url}`); 
+  console.log(`sending ${encryptedData} to ${url}`);
 }
 
-
-module.exports = {
-    send,
-}
+// module.exports = {
+//   REQUEST_TIMEOUT,
+//   send,
+// };
