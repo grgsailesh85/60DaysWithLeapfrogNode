d9d2d342377c2de28e27d8885c779275e1203dca
Sailesh Gurung <saileshgurung98@gmail.com>
2025-06-17 22:39:06 +0545

    day 17

--- Day17/index.js
+++ Day17/index.js
@@ -18,22 +18,17 @@ const server = http.createServer((req, res) => {
     },
   ];
   server.on("request", (req, res) => {
-    if (req.url === "/") {
-      // res.writeHead(200, {
-      // "Content-Type": "text/plain",
-      //   "Content-Type": "application/json",
-      // });
+    const items = req.url.split("/"); // /friends/2 => ['', 'friends', '2']
+    if (items[1] === "friends") {
       res.statusCode = 200;
       res.setHeader("Content-Type", "application/json");
-
-      //   res.end("Hello! My name is Sailesh Gurung");
-      res.end(
-        JSON.stringify({
-          id: 1,
-          name: "Saileh Gurung",
-        })
-      );
-    } else if (req.url === "/messages") {
+      if (items.length === 3) {
+        const friendIndex = Number(items[2]);
+        res.end(JSON.stringify(friends[friendIndex]));
+      } else {
+        res.end(JSON.stringify(friends));
+      }
+    } else if (items[1] === "messages") {
       res.setHeader("Content-Type", "text/html");
       res.write("<html>");
       res.write("<body>");
@@ -51,5 +46,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Lsitening on port ${PORT}.....`);
+  console.log(`Listening on port ${PORT}.....`);
 });
