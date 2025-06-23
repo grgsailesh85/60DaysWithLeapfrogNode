760853383efb6bc334370e9fd516c1b674425127
Sailesh Gurung <saileshgurung98@gmail.com>
2025-06-23 22:33:24 +0545

    day 23

---
+++ Day23/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const friendsController = require("./controllers/friends.controller");
+
+const messagesController = require("./controllers/messages.controller");
+
+const app = express();
+
+const PORT = 3000;
+
+
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post("/friends", friendsController.postFriend);
+app.get("/friends", friendsController.getFriends);
+// GET /friends/22
+app.get("/friends/:friendId", friendsController.getFriend);
+
+
+app.get("/messages", messagesController.getMessages);
+app.post("/messages", messagesController.postMessages);
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
