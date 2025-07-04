3540e0a727651857aeba0e0a7bf401d3e676bae1
Sailesh Gurung <saileshgurung98@gmail.com>
2025-07-04 22:47:39 +0545

    day34

--- Project02/server/src/routes/launches/launches.controller.js
+++ Project02/server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
+const { httpAbortLaunch } = require("../../../../client/src/hooks/requests");
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+} = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -6,9 +11,41 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
   addNewLaunch(launch);
   return res.status(201).json(launch);
+}
+
+function httpAbortLaunch(req, res) {
+  const launchId = req.params.id;
+
+  if (existsLaunchWithId(launchId)) {
+    // if launch doesnot exist
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+
+  // if launch does exist
+  return res.status(200).json(aborted);
 }
 
 module.exports = {
