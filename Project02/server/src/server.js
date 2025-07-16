f462cfca1fb7f02027608ba7b16ecd9071b284ff
Sailesh Gurung <saileshgurung98@gmail.com>
2025-07-16 23:32:29 +0545

    day 46

--- Project02/server/src/server.js
+++ Project02/server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,9 +7,24 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:9yTgYBAtn85XReUc@cluster0.hhh5gle.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0";
+
 const server = http.createServer(app);
 
+mongoose.connection.on('open', () => {
+  console.log('MongoDB connection established successfully')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err)
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
